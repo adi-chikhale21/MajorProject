bd2e783dc1bbb8e5df61c9d313a26a9df9931f79
Aditya Chikhale <adityachikhale19@gmail.com>
2024-12-22 23:33:33 +0530

    update mongo url

--- init/index.js
+++ init/index.js
@@ -4,23 +4,26 @@ const initData = require("./data.js");
 
 mongoose_URL = "mongodb://127.0.0.1:27017/Wanderlust";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("DB is connected");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(mongoose_URL);
+  await mongoose.connect(mongoose_URL);
 }
 
-
-const Data = async () =>{
-    await listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "65e0ae24fd141700d3034cd9"}))
-    await listing.insertMany(initData.data);
-    console.log("Data was saved")
-
+const Data = async () => {
+  await listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65e0ae24fd141700d3034cd9",
+  }));
+  await listing.insertMany(initData.data);
+  console.log("Data was saved");
 };
 
 Data();
